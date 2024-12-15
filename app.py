c2c1dff36964e769d836b54cceec3f9bc413dc0c
Elias <elabd.elias@gmail.com>
2024-12-15 11:33:06 +0100

    [Elias] Converted app to oop for future features

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from tkinter import PhotoImage
 import customtkinter
 
-# This file contains basic settings
+# This file contains settings
 # for the application
 
 # System settings
 # Identical as user's theme
 customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
 
-# Create the app frame
-app = customtkinter.CTk()
-app.geometry("720x480")
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Customize app frame
-app.title("PyPlayer")
-icon = PhotoImage(file="assets/music-notes.png")
-app.after(201, lambda :app.iconphoto(False, icon))
+        # Customize app frame
+        self.title("PyPlayer")
+        icon = PhotoImage(file="assets/music-notes.png")
+        self.after(201, lambda :self.iconphoto(False, icon))
+        self.geometry("720x480")
 
-# Run the app
+# Create and run the app
+app = App()
 app.mainloop()
