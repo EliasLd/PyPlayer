07b2398cd0ad89399643e8893c77a13d3dfe2d2c
Elias <elabd.elias@gmail.com>
2024-12-15 13:24:44 +0100

    [Elias] remove moved files

--- music.py
+++
@@ -1,6 +0,0 @@
-class Music():
-    def __init__(self, title, author, duration, cover):
-        self.title = title
-        self.author = author
-        self.duration = duration
-        self.cover = cover
