3f22fe820a342cf0093494782c498b5b06b3b82b
Elias <elabd.elias@gmail.com>
2024-12-15 11:35:04 +0100

    [Elias] Add music classe

---
+++ music.py
@@ -0,0 +1,6 @@
+class Music():
+    def __init__(self, title, author, duration, cover):
+        self.title = title
+        self.author = author
+        self.duration = duration
+        self.cover = cover
